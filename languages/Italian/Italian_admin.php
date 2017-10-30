1efea84a1db7937f54c1f40e3390ca21492ecb06
kreos <kreos@serverkreos.com>
2017-10-30 17:48:17 +0100

    + Italian language

---
+++ languages/Italian/Italian_admin.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * EU cookies plugin for CMS e107 v2
+ *
+ * @author OxigenO2 (oxigen.rg@gmail.com)
+ * @copyright Copyright (C) 2015 OxigenO2 
+ * @license GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * @link http://oxigen.mablog.eu/
+ */
+define("LAN_PLUGIN_EUC_STYLE", "Stile");
+define("LAN_PLUGIN_EUC_TEXT", "Testo per il banner");
+define("LAN_PLUGIN_EUC_POLICYLINK", "Link per estendere le policy dei cookie");
+define("LAN_PLUGIN_EUC_BTN", "Questo é il testo per il bottone di conferma.");
+define("LAN_PLUGIN_EUC_POSITION", "Posizioni  banner");
+define("LAN_PLUGIN_EUC_THEME", "Colore del banner");
+define("LAN_PLUGIN_EUC_OPENEFFECT", "Effetto apertura del banner dei cookie.");
+define("LAN_PLUGIN_EUC_OPENEFFECTDURATION", "durata per effetto di apertura del banner in msec");
+define("LAN_PLUGIN_EUC_OPENEFFECTEASING", "funzionalità apertura banner");
+define("LAN_PLUGIN_EUC_CLOSEEFFECT", "effetto di chiusura");
+define("LAN_PLUGIN_EUC_CLOSEEFFECTDURATION", "durata effetto di chiusura del banner in msec");
+define("LAN_PLUGIN_EUC_CLOSEEFFECTEASING", "funzionalità di chusura del banner");
+define("LAN_PLUGIN_EUC_FADE",  "dissolvenza");
+define("LAN_PLUGIN_EUC_SLIDEDOWN",  "scorri in basso");
+define("LAN_PLUGIN_EUC_SLIDEUP",  "scorri in alto");
+define("LAN_PLUGIN_EUC_SLIDELEFT",  "scorri a sinistra");
+define("LAN_PLUGIN_EUC_SLIDERIGHT",  "scorri a destra");
+define("LAN_PLUGIN_EUC_TOP",  "Alto");
+define("LAN_PLUGIN_EUC_BOTTOM",  "Basso");
+define("LAN_PLUGIN_EUC_DARK",  "Scuro");
+define("LAN_PLUGIN_EUC_LIGHT",  "Chiaro");
+define("LAN_PLUGIN_EUC_SWING",  "Oscillante");
+define("LAN_PLUGIN_EUC_LINEAR",  "Lineare");
+?>
